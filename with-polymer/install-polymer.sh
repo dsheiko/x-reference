11380947f724cca176cfc54821de949559ca19c2
Dmitry Sheiko <web.sbelarus@gmail.com>
2014-09-03 18:00:43 +0200

    polymer core-components removed from repo, installer script added instead

---
+++ with-polymer/install-polymer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://github.com/Polymer/platform.git components/platform 
+git clone https://github.com/Polymer/core-meta.git components/core-meta
+git clone https://github.com/Polymer/core-transition.git components/core-transition
+git clone https://github.com/Polymer/core-overlay.git components/core-overlay
