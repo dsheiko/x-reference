ba20d880aceca512a98158529c1b56b4446ff328
Dmitry Sheiko <web.sbelarus@gmail.com>
2014-09-05 08:32:19 +0200

    refining following boilerplates

--- with-polymer/install-polymer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone https://github.com/Polymer/platform.git components/platform 
-git clone https://github.com/Polymer/core-meta.git components/core-meta
-git clone https://github.com/Polymer/core-transition.git components/core-transition
-git clone https://github.com/Polymer/core-overlay.git components/core-overlay
-git clone https://github.com/Polymer/polymer components/polymer
